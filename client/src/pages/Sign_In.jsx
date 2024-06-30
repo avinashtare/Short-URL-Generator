d234188bb88ba0623eb01a7765176ecf16c6406e
Avinash Tare <avinashtare5@gmail.com>
2024-07-01 00:58:28 +0530

    fixed alies path

--- client/src/pages/Sign_In.jsx
+++ client/src/pages/Sign_In.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import SignInForm from '../components/sign_in/SignInForm'
+import SignInForm from '@/components/sign_in/SignInForm'
 
 const Sign_In = () => {
     return (
