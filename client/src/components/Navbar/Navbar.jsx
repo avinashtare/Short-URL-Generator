edca0e3ca420bee30d2cd5f06737712a910f575f
Avinash Tare <avinashtare5@gmail.com>
2024-07-01 00:13:29 +0530

    fixed: name email undefind error

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -86,8 +86,8 @@ const Navbar = () => {
             id="user-dropdown"
           >
             <div className="px-4 py-3">
-              <span className="block text-sm text-gray-900 dark:text-white">{userState.userInfo.fullName}</span>
-              <span className="block text-sm text-gray-500 truncate dark:text-gray-400">{userState.userInfo.email}</span>
+              <span className="block text-sm text-gray-900 dark:text-white">{userState.userInfo?.fullName}</span>
+              <span className="block text-sm text-gray-500 truncate dark:text-gray-400">{userState.userInfo?.email}</span>
             </div>
             <ul className="py-2" aria-labelledby="user-menu-button">
               {
