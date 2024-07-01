3bfcbf15e980e42c01c05d7351e92a895939e49e
Avinash Tare <avinashtare5@gmail.com>
2024-07-01 21:53:41 +0530

    fiexd: url triming

--- client/src/components/Link_Box/Link_Form.jsx
+++ client/src/components/Link_Box/Link_Form.jsx
@@ -26,7 +26,7 @@ function Link_Form() {
             navigate("/sign_up")
             return null;
         }
-        let UrlResponse = await dispatch(createUrl({ url: CreateUrl }))
+        let UrlResponse = await dispatch(createUrl({ url: CreateUrl.trim() }))
         let UrlResponseData = UrlResponse.payload;
         if (UrlResponseData?.isValidUrl == false) {
             toast.warning("Invalid Url")
@@ -76,7 +76,7 @@ function Link_Form() {
                             <div className="relative w-full">
                                 <input type="url" value={CreateUrl} onChange={((e) => setCreateUrl(e.target.value))} className="block p-3 w-full z-20 text-gray-900 bg-gray-50 rounded-lg border-s-gray-50 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-s-gray-700  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500 text-base" placeholder="Enter the link here.." required />
 
-                                <button type="submit" className="absolute color-white top-0 end-0 p-2.5 text-base font-medium h-full text-white bg-blue-700 rounded-e-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                <button type="submit" className="absolute color-white top-0 end-0 p-2.5 text-base font-medium h-full text-white bg-blue-700 rounded-e-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 disabled" disabled={urlsState.isLoading}>
                                     <span>{urlsState.isLoading ? "Generating.." : "Shorten URL"}</span>
                                 </button>
                             </div>
